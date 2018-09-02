645d68f8488388ee4c01a414b596cdb0fd333a77
Jake Levi <jakelevi1996@gmail.com>
2018-09-03 00:00:04 +0100

    Add note about loading model for continued training

--- Code/training.py
+++ Code/training.py
@@ -1,21 +1,28 @@
-print("Starting imports...")
-import tensorflow as tf
-print("Imported tf")
-import time
 import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("Importing tensorflow...")
+import tensorflow as tf
+import time
 from data.data_util import load_data, DEFAULT_FILENAME
 from models.classifier import NeuralClassifier
-print("Finished imports")
 
-DEFAULT_SAVE_DIR = \
-    "models/" + time.strftime("%Y.%m.%d-%H.%M.%S-") + "saved_model"
+timestamp = time.strftime("%Y.%m.%d-%H.%M.%S-")
+DEFAULT_SAVE_DIR = "models/" + timestamp + "saved_model"
+DEFAULT_LOG_DIR = "logs"
+
+def display_progress(epoch, loss_val):
+    logging.info("Epoch: {:<8} | Loss: {:<.6f}".format(epoch, loss_val))
+
 
 def train(
-    num_epochs=2000,
-    print_every=100,
+    num_epochs=5000,
+    print_every=1000,
+    model_name="standard_model",
     data_filename=DEFAULT_FILENAME,
-    savedir=DEFAULT_SAVE_DIR
-):
+    savedir=DEFAULT_SAVE_DIR,
+    logdir=DEFAULT_LOG_DIR
+    ):
+    # TODO: add option to load model for continued training
     logging.info("Loading data...")
     x_train, y_train, x_test, y_test = load_data(data_filename)
     logging.info("Creating model...")
@@ -24,7 +31,8 @@ def train(
 
     with tf.Session() as sess:
         model.initialize_variables(sess)
-        for e in range(num_epochs):
+        for epoch in range(num_epochs):
+            # Evaluate graph, summaries, and training op
             loss_val, summary_val, _ = sess.run(
                 (model.loss_op,
                 model.merged_summary_op,
@@ -32,9 +40,12 @@ def train(
                 feed_dict={
                     model.input_placeholder: x_train,
                     model.output_placeholder: y_train})
-            print("Epoch: {:<8} | Loss: {:<.6f}".format(e, loss_val))
+            if epoch % print_every == 0:
+                display_progress(epoch, loss_val)
+
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    train(num_epochs=10,print_every=1)
+    # train(num_epochs=10,print_every=1)
+    train()
